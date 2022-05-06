8cf8878952fb14cbc2400a77e429479daf7b1694
enzo grillo <enzogrillo96@gmail.com>
2022-05-06 14:47:16 +0200

    first

---
+++ public/js/main.js
@@ -0,0 +1,82 @@
+//exo1
+
+// let tableau = ["tommy", "johnny", "edward", "paul", "george", "nathan", "james","alex","fed"];
+// let i = 0 ;
+
+
+// while (i < 9){
+//     console.log("Bonjour " + tableau[i] + "." + i);
+//     i++ ; 
+// }
+
+//exo2
+
+// let fruits = ["apple", "banana", "cherry", "blueberry", "orange", "mango"];
+// let i = fruits.length;
+
+// while (fruits.length){
+//     fruits.pop();
+//     console.log(fruits); 
+// }
+
+//exo3
+
+// let panierlegume = ["aubergine", "tomate", "courgette", "pomme-de-terre"];
+// let longueur = panierlegume.length -1;
+// let caisselegume = [];
+
+
+//  while (longueur >= 0){
+//      caisselegume.push(panierlegume[longueur]);
+//      panierlegume.splice(longueur, 1);
+//      longueur--;
+       
+//  }
+//  console.log(caisselegume);
+//  console.log(panierlegume);
+
+//exo 4
+
+
+// let answer = "man";
+
+// while (answer === "man"){
+//     let riddle = prompt("What starts life with 4 legs than 2 than 3 ?");
+//     if (riddle === answer){
+//         alert("noice");
+//         answer = sdfdsf;
+//     }else {
+//         alert("nop");
+//     }
+    
+// }
+
+//exo 5
+
+// let answer = "man";
+
+// do {
+//     let riddle = prompt("What starts life with 4 legs than 2 than 3 ?");
+//     if (riddle === answer){
+//        alert("noice");
+//        answer = sdfdsf;
+//     }else {
+//         alert("nop");
+//    }
+   
+
+// } while (answer === "man") 
+
+
+//exo 6
+
+let year = 2022
+
+
+
+
+
+
+
+
+
